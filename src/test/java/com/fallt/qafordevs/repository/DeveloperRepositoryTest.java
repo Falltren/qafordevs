93399f4a25db5daea3e010411d8b32bb07b39f84
Andrei Guzei <aaguzey@gmail.com>
2024-07-26 14:57:16 +1000

    added tests for testing DeveloperRepository methods added util class for creating developers

--- src/test/java/com/fallt/qafordevs/repository/DeveloperRepositoryTest.java
+++ src/test/java/com/fallt/qafordevs/repository/DeveloperRepositoryTest.java
@@ -1,11 +1,23 @@
 package com.fallt.qafordevs.repository;
 
+import com.fallt.qafordevs.entity.DeveloperEntity;
+import com.fallt.qafordevs.util.DataUtils;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.util.CollectionUtils;
+
+import java.text.MessageFormat;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.LIST;
 
 @DataJpaTest
-public class DeveloperRepositoryTest {
+class DeveloperRepositoryTest {
 
     @Autowired
     private DeveloperRepository developerRepository;
@@ -14,4 +26,96 @@ public class DeveloperRepositoryTest {
     public void setUp() {
         developerRepository.deleteAll();
     }
+
+    @Test
+    @DisplayName("Test save developer functionality")
+    void givenDeveloperObject_whenSave_thenDeveloperIsCreated() {
+        //given
+        DeveloperEntity developerToSave = DataUtils.getJohnDoeTransient();
+        //when
+        DeveloperEntity savedDeveloper = developerRepository.save(developerToSave);
+        //then
+        assertThat(savedDeveloper).isNotNull();
+        assertThat(savedDeveloper.getId()).isNotNull();
+    }
+
+    @Test
+    @DisplayName("Test update developer functionality")
+    void givenDeveloperToUpdate_whenSave_thenEmailIsChanged() {
+        //given
+        String updatedEmail = "updated@gmail.com";
+        DeveloperEntity developerToCreate = DataUtils.getJohnDoeTransient();
+        developerRepository.save(developerToCreate);
+        //when
+        Optional<DeveloperEntity> optionalDeveloper = developerRepository.findById(developerToCreate.getId());
+        if (optionalDeveloper.isEmpty()) {
+            throw new RuntimeException("Incorrect ID");
+        }
+        DeveloperEntity developerToUpdate = optionalDeveloper.get();
+        developerToUpdate.setEmail(updatedEmail);
+        DeveloperEntity updatedDeveloper = developerRepository.save(developerToUpdate);
+        //then
+        assertThat(updatedDeveloper).isNotNull();
+        assertThat(updatedDeveloper.getEmail()).isEqualTo(updatedEmail);
+    }
+
+    @Test
+    @DisplayName("Test get developer by id functionality")
+    void givenDeveloperCreated_whenGetById_thenDeveloperReturn() {
+        //given
+        DeveloperEntity developerToSave = DataUtils.getJohnDoeTransient();
+        developerRepository.save(developerToSave);
+        //when
+        Optional<DeveloperEntity> optionalDeveloper = developerRepository.findById(developerToSave.getId());
+        if (optionalDeveloper.isEmpty()) {
+            throw new RuntimeException("Incorrect ID");
+        }
+        DeveloperEntity obtainedDeveloper = optionalDeveloper.get();
+        //then
+        assertThat(obtainedDeveloper).isNotNull();
+        assertThat(obtainedDeveloper.getEmail()).isEqualTo("john.doe@gmail.com");
+    }
+
+    @Test
+    @DisplayName("Test developer not found functionality")
+    void givenDeveloperIsNotCreated_whenGetById_thenOptionalIsEmpty() {
+        //given
+        //when
+        Optional<DeveloperEntity> optionalDeveloper = developerRepository.findById(1);
+        //then
+        assertThat(optionalDeveloper).isEmpty();
+    }
+
+    @Test
+    @DisplayName("Test get all developers functionality")
+    void givenThreeDevelopersAreStored_whenFindAll_thenAllDevelopersAreReturned() {
+        //given
+        DeveloperEntity developer1 = DataUtils.getJohnDoeTransient();
+        DeveloperEntity developer2 = DataUtils.getMikeSmithTransient();
+        DeveloperEntity developer3 = DataUtils.getFrankJonesTransient();
+        developerRepository.saveAll(List.of(developer1, developer2, developer3));
+        //when
+        List<DeveloperEntity> obtainedDevelopers = developerRepository.findAll();
+        //then
+        assertThat(CollectionUtils.isEmpty(obtainedDevelopers)).isFalse();
+        assertThat(obtainedDevelopers).hasSize(3);
+    }
+
+    @Test
+    @DisplayName("Test get developer by email functionality")
+    void givenDeveloperSaved_whenGetByEmail_thenDeveloperIsReturned() {
+        //given
+        DeveloperEntity developer = DataUtils.getJohnDoeTransient();
+        developerRepository.save(developer);
+        //when
+        String email = "john.doe@gmail.com";
+        Optional<DeveloperEntity> optionalDeveloper = developerRepository.findByEmail(email);
+        if (optionalDeveloper.isEmpty()) {
+            throw new RuntimeException(MessageFormat.format("Developer with email {0} does not exist", email));
+        }
+        DeveloperEntity obtainedDeveloper = optionalDeveloper.get();
+        //then
+        assertThat(obtainedDeveloper.getEmail()).isEqualTo(email);
+    }
+
 }
---
+++ src/test/java/com/fallt/qafordevs/util/DataUtils.java
@@ -0,0 +1,70 @@
+package com.fallt.qafordevs.util;
+
+import com.fallt.qafordevs.entity.DeveloperEntity;
+import com.fallt.qafordevs.entity.Status;
+
+public class DataUtils {
+
+    public static DeveloperEntity getJohnDoeTransient() {
+        return DeveloperEntity.builder()
+                .firstName("John")
+                .lastName("Doe")
+                .email("john.doe@gmail.com")
+                .speciality("Java")
+                .status(Status.ACTIVE)
+                .build();
+    }
+
+    public static DeveloperEntity getJohnDoePersisted() {
+        return DeveloperEntity.builder()
+                .id(1)
+                .firstName("John")
+                .lastName("Doe")
+                .email("john.doe@gmail.com")
+                .speciality("Java")
+                .status(Status.ACTIVE)
+                .build();
+    }
+
+    public static DeveloperEntity getMikeSmithTransient(){
+        return DeveloperEntity.builder()
+                .firstName("Mike")
+                .lastName("Smith")
+                .email("mike.smith@gmail.com")
+                .speciality("Java")
+                .status(Status.ACTIVE)
+                .build();
+    }
+
+    public static DeveloperEntity getMikeSmithPersisted(){
+        return DeveloperEntity.builder()
+                .id(2)
+                .firstName("Mike")
+                .lastName("Smith")
+                .email("mike.smith@gmail.com")
+                .speciality("Java")
+                .status(Status.ACTIVE)
+                .build();
+    }
+
+    public static DeveloperEntity getFrankJonesTransient(){
+        return DeveloperEntity.builder()
+                .firstName("Frank")
+                .lastName("Jones")
+                .email("frank.jones@gmail.com")
+                .speciality("Java")
+                .status(Status.ACTIVE)
+                .build();
+    }
+
+    public static DeveloperEntity getFrankJonesPersisted(){
+        return DeveloperEntity.builder()
+                .id(3)
+                .firstName("Frank")
+                .lastName("Jones")
+                .email("frank.jones@gmail.com")
+                .speciality("Java")
+                .status(Status.ACTIVE)
+                .build();
+    }
+}
